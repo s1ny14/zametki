322dffd7df495f56f16960e02562aba3ef87edb3
Софья Жданова <sonyazhdanovaaa@gmail.com>
2025-11-09 20:24:44 +0300

    удаляем файл коммандс, так как будет интерфейс

--- notebook/__init__.py
+++ notebook/__init__.py
@@ -1,13 +1,7 @@
 from .models import Note
 from .storage import Storage
-from .commands import setup_parser, add_note, list_notes, search_notes, delete_note
 
 __all__ = [
     "Note",
     "Storage",
-    "setup_parser",
-    "add_note",
-    "list_notes",
-    "search_notes",
-    "delete_note"
 ]
